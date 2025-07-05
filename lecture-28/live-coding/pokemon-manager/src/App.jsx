449568cdcac55fb8b7531cd57ec6efbdabab04dd
Nikhilnair48 <nikhil.nair48@gmail.com>
2025-07-05 20:07:40 +0530

    feat: pokemon listing initial version

---
+++ lecture-28/live-coding/pokemon-manager/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import PokemonLister from "./components/PokemonLister";
+
+function App() {
+  return (
+      <PokemonLister />
+  )
+}
+
+export default App
